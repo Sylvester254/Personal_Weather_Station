ba7bd368c6359ceec38f045b14f70ae5e7d5ba66
Sylvester254 <slynganga59@gmail.com>
2023-03-04 19:44:46 +0300

    recorded at...

--- Temp_humidity.py
+++ Temp_humidity.py
@@ -1,18 +1,26 @@
 from machine import Pin
 from PicoDHT22 import PicoDHT22
 import time
+import utime
 
 """This code should continuously read the temperature
-    and humidity values and print them to the console
-    with a 5-second delay between each reading
+    and humidity values and print them to the shell
+    with a 10-second delay between each reading.
+    It also displays the current time when the readings were recorded
 """
 
-
 # init DHT22 on Pin 2(GPIO2)
-dht22 = PicoDHT22(Pin(2,Pin.IN,Pin.PULL_UP))
+dht22 = PicoDHT22(Pin(2, Pin.IN, Pin.PULL_UP))
 while True:
     T, H = dht22.read()
-    print("\n Reading: ")
+    # Get current time in seconds since the Epoch
+    now = utime.time()
+    # Convert to a tuple of (year, month, day, hour, minute, second, weekday, yearday)
+    time_tuple = utime.gmtime(now)
+    # Format the time as a string
+    time_str = "{:04}-{:02}-{:02} {:02}:{:02}:{:02}".format(time_tuple[0], time_tuple[1], time_tuple[2], time_tuple[3], time_tuple[4], time_tuple[5])
+
+    print("\n Reading at {} : ".format(time_str))
     print("Temperature: {} °C".format(T))
     print("Humidity: {} %".format(H))
-    time.sleep(5)  # wait for 5 seconds before reading again
+    time.sleep(10)  # wait for 10 seconds before reading again
